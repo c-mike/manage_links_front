80cdb8fc06cc35d22384eba085c227d448b300d0
c-mike <claudiomichael83@gmail.com>
2023-01-15 11:41:22 -0100

    index page to list, edit, delete links

---
+++ front/static/js/index.js
@@ -0,0 +1,26 @@
+const URL = 'http://localhost:8888';
+
+function edit() {
+    const id = parseInt(document.getElementById('id_edit').value);
+    const title = document.getElementById('title').value;
+    const url = document.getElementById('url').value;
+
+    const data = {id, title, url}
+
+    axios.post(URL+'/edit', data)
+    .then(()=> window.location.reload()) 
+    .catch(errors => {
+        console.log(errors)
+        document.getElementById('url').hidden = false
+    })
+}
+
+function del() {
+    const id = parseInt(document.getElementById('id_edit').value);
+    const data = {id}
+    console.log(data)
+
+    axios.delete(URL+'/remove', {data})
+    .then(()=> window.location.reload()) 
+    .catch(errors => console.log(errors))
+}
--- front/views.py
+++ front/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 
+URL = 'http://localhost:8888'
+
 def index(request):
-    # response = requests.get(URL + 'school/')
-    # indexs = response.json()
-    return render(request, 'index.html') #,{'indexs':indexs}
+    response = requests.get(URL + '/all')
+    links = response.json()
+    return render(request, 'index.html',{'links':links})
