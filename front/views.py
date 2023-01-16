15fb70a8bc7cf1f29e8603fc59ae5d3afa53f9c4
c-mike <claudiomichael83@gmail.com>
2023-01-16 14:45:13 -0100

    web crawler

--- front/views.py
+++ front/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import requests
-
-URL = 'http://localhost:8888'
+from decouple import config
+import re
 
 def index(request):
-    response = requests.get(URL + '/all')
+    response = requests.get( config('API')+ '/all')
     links = response.json()
+    urls = []
+
+    if request.method == "POST":
+        url = request.POST.get("input_link")
+
+        r = requests.get(url)
+        print('r ', r)
+
+        html = r.text.encode('utf8')
+        print('html ', html)
+
+        search = re.findall(r'<a href=[\'"?](https[://\w\-._]+)', html.decode('utf8'))
+        print('html ', html)
+
+        for link in search:
+            if link not in urls:
+                urls.append(link)
+                print(link)  
+
     return render(request, 'index.html',{'links':links})
